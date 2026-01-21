75c1d9809b6b2038db32eb93211ddbe1d97d64ad
Omoniyi Tomiwa <tomiwaomoniyi4@gmail.com>
2026-01-21 19:47:45 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'aaua-mart',
+  webDir: 'www'
+};
+
+export default config;
